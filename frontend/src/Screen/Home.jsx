2816153a3dfcdf2e23a66d4e797c0054244fb879
SunEom <dkxl0701@gmail.com>
2021-10-02 14:19:33 +0900

    Updated Home Screen

---
+++ frontend/src/Screen/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Hello = styled.div`
+  margin: 100px;
+`;
+
+const Hello2 = styled.button`
+  padding: 20px;
+  margin-left: 10px;
+`;
+
+const Home = () => {
+  return (
+    <>
+      <Hello>Home</Hello>
+      <Hello2>Hello</Hello2>
+    </>
+  );
+};
+
+export default Home;
