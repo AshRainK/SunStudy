a9a6e8c415292b76fb31ac107b319ab2304c03c4
SunEom <dkxl0701@gmail.com>
2021-11-11 10:57:09 +0900

    Created Login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import React from 'react';
+import Login from './Screen/Login';
 
-import Home from './Screen/Home';
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  font-size: 100%;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  height: 180px;
+`;
+
+const Login_container = styled.div`
+  margin-top: 10px;
+  margin-right: 20px;
+  display: flex;
+  justify-content: right;
+  position: relative;
+`;
+
+const Login_textb = styled.button`
+  font-size: 15px;
+  color: white;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 500;
+`;
+
+const Header_logo = styled.div`
+  position: relative;
+  top: -14px;
+  display: flex;
+  width: 100%;
+  justify-content: left;
+  align-items: center;
+  margin-left: 45px;
+`;
+
+const Header_title_container = styled.div`
+  display: flex;
+  height: 130px;
+  width: 130px;
+  border: 5px solid white;
+`;
+
+const Header_title = styled.div`
+  margin: 15px 5px;
+  font-size: 25px;
+  font-weight: 600;
+  color: white;
+  font-family: 'Rock Salt', cursive;
+  line-height: 1.35em;
+`;
+
+const Search = styled.div`
+  top: -50px;
+  right: 20px;
+  display: flex;
+  justify-content: right;
+  position: relative;
+`;
+
+const Search_bar = styled.input`
+  width: 15%;
+  height: 38px;
+  position: relative;
+  border: none;
+  background: transparent;
+  outline: none;
+  color: white;
+  caret-color: white;
+  border-bottom: 2px solid white;
+  font-size: 15px;
+`;
+
+const Search_btn = styled.button`
+  background: transparent;
+  color: white;
+  position: absolute;
+  right: 0.5%;
+  top: 10px;
+  font-size: 18px;
+  border: none;
+  cursor: pointer;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
-  return <Home></Home>;
+  return (
+    <div className="App">
+      <Body>
+        <Header>
+          <Login_container>
+            <Login_textb>Login</Login_textb>
+          </Login_container>
+          <Header_logo>
+            <Header_title_container>
+              <Header_title>My Music Record.</Header_title>
+            </Header_title_container>
+          </Header_logo>
+          <Search>
+            <Search_bar type="text" placeholder="Search" />
+            <Search_btn type="submit">
+              <i class="fas fa-search"></i>
+            </Search_btn>
+          </Search>
+        </Header>
+        <Main>
+          <Login />
+        </Main>
+      </Body>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Screen/Login.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Login_page_container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: middle;
+  align-items: center;
+  padding: 20px 10px;
+  width: 500px;
+  height: 700px;
+`;
+
+const Login_text = styled.div`
+  margin: 70px 0;
+  font-size: 24px;
+  font-weight: 1000;
+`;
+
+const Login_form_container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 260px;
+`;
+
+const Input_ID = styled.input`
+  width: 400px;
+  height: 50px;
+  border: 1px solid lightgrey;
+  outline: none;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  &:focus {
+    border: 1px solid grey;
+  }
+`;
+
+const Input_PW = styled.input`
+  width: 400px;
+  height: 50px;
+  border: 1px solid lightgrey;
+  outline: none;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  &:focus{
+    border: 1px solid grey;d
+`;
+
+const Login_btn = styled.button`
+  width: 413px;
+  height: 50px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 15px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Login_help = styled.div`
+  display: flex;
+  width: 413px;
+  height: 40px;
+  margin-top: 10px;
+`;
+
+const ID_remember_checkbox = styled.input`
+  width: 18px;
+  height: 18px;
+  border: 1px solid #ccc;
+  content: '';
+`;
+
+const ID_remember = styled.div`
+  font-weight: 800;
+  font-size: 15px;
+`;
+
+const Signup_btn = styled.button`
+  width: 413px;
+  height: 50px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 15px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Login = () => {
+  return (
+    <Login_page_container>
+      <Login_text>Login</Login_text>
+      <Login_form_container>
+        <Input_ID type="text" placeholder="아이디 입력" />
+        <Input_PW type="text" placeholder="패스워드 입력" />
+        <Login_btn>로그인</Login_btn>
+        <Login_help>
+          <ID_remember_checkbox type="checkbox" />
+          <ID_remember>아이디 기억</ID_remember>
+        </Login_help>
+      </Login_form_container>
+      <Signup_btn>회원가입</Signup_btn>
+    </Login_page_container>
+  );
+};
+
+export default Login;
