cc715044d8b9c49769e32c32e2a3ae8e0da4d252
JW <kjo98@naver.com>
2021-11-22 23:53:49 +0900

    add comment router and modify ohter router files

--- backend/router/search.js
+++ backend/router/search.js
@@ -5,7 +5,7 @@ const db = require('../lib/database');
 router.get('/',(req,res,next)=>{
     const body = req.body;
     let word = "%" + body.keyword + "%";
-    db.query(`SELECT post.post_num,title,post_body,created_date,post.updated_date,post.id as writer_id,count(comment.post_num) as comment_count FROM post LEFT JOIN comment on post.post_num = comment.post_num GROUP BY post_num having title LIKE ? ORDER BY post.created_date DESC`,
+    db.query(`SELECT post.post_num,title,post_body,created_date,post.updated_date,post.id as writer_id,post.genre,count(comment.post_num) as comment_count FROM post LEFT JOIN comment on post.post_num = comment.post_num GROUP BY post_num having title LIKE ? ORDER BY post.created_date DESC`,
     [word],
     (err,result)=>{
         if(err){
