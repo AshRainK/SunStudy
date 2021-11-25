a68869b31d016a9eedd079eb41bdac116f260237
unknown <kjo98@naver.com>
2021-11-25 21:09:20 +0900

    add artist reference & create mypage router

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const authRouter = require('./router/auth');
 const regiRouter = require('./router/register');
 const searchRouter = require('./router/search');
 const commentRouter = require('./router/comment');
+const mypageRouter = require('./router/mypage');
 
 app.use(cors());
 app.set('port', process.env.PORT || 8000);
@@ -40,6 +41,7 @@ app.use('/auth',authRouter);
 app.use('/register',regiRouter);
 app.use('/search',searchRouter);
 app.use('/comment',commentRouter);
+app.use('/mypage',mypageRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
---
+++ backend/router/mypage.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('../lib/database');
+const router = express.Router();
+const session = require('express-session');
+const saltRounds = 10;
+const passport = require('passport');
+
+router.patch("/update/aboutme",(req,res,next)=>{
+    const aboutme = req.body.aboutme;
+    db.query(`UPDATE user SET about_me = ? WHERE id = ?`,
+    [aboutme,req.user.id],
+    (err)=>{
+        if(err){
+            next(err);
+        }
+        db.query(`SELECT id,user_id,nickname,about_me FROM user WHERE id = ?`,
+        [req.user.id],
+        (err,user_result)=>{
+            if(err){
+                next(err);
+            }
+            db.query('SELECT genre FROM genre WHERE id = ?',
+            [req.user.id],
+            (err,genre_result)=>{
+                if(err){
+                    next(err);
+                }
+                res.status(200).send({code : 200, payload : {user_data : user_result[0], genre_result}});
+            });
+        });
+    });
+});
+
+module.exports = router;
