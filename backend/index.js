63ad2ee14075cd2892fcec6fb4d9d7e3e1eed276
Suneom <dkxl0701@gmail.com>
2022-01-09 14:13:24 +0900

    Fixed CORS error

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const passportconfig = require('./lib/passport/passport');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const passportconfig = require("./lib/passport/passport");
 passportconfig();
 const app = express();
 
-const PostRouter = require('./router/post');
-const authRouter = require('./router/auth');
-const regiRouter = require('./router/register');
-const searchRouter = require('./router/search');
-const commentRouter = require('./router/comment');
-const mypageRouter = require('./router/mypage');
+const PostRouter = require("./router/post");
+const authRouter = require("./router/auth");
+const regiRouter = require("./router/register");
+const searchRouter = require("./router/search");
+const commentRouter = require("./router/comment");
+const mypageRouter = require("./router/mypage");
 
-app.use(cors());
-app.set('port', process.env.PORT || 8000);
+app.use(cors({ origin: true, credentials: true }));
+app.set("port", process.env.PORT || 8000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -36,12 +36,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/post', PostRouter);
-app.use('/auth',authRouter);
-app.use('/register',regiRouter);
-app.use('/search',searchRouter);
-app.use('/comment',commentRouter);
-app.use('/mypage',mypageRouter);
+app.use("/post", PostRouter);
+app.use("/auth", authRouter);
+app.use("/register", regiRouter);
+app.use("/search", searchRouter);
+app.use("/comment", commentRouter);
+app.use("/mypage", mypageRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -52,9 +52,9 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   //에러핸들러
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!');
+  console.log("Example app listening on port 8000!");
 });
