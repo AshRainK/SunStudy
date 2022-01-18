41dcdf6c0b1c405e4c39a70caebd953b57d761a1
Mihee <vip0020@naver.com>
2022-01-18 18:31:50 +0900

    포스트 업로드 기능 구현

--- frontend/src/Screen/Postingpage.jsx
+++ frontend/src/Screen/Postingpage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
 import styled from "styled-components";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Posting_page = styled.div`
   background-color: #f5f5f5;
@@ -106,6 +107,72 @@ const Submit_btn = styled.button`
 `;
 
 const Postingpage = () => {
+  const [title, setTitle] = useState("");
+  const [genre, setGenre] = useState("");
+  const [artist, setArtist] = useState("");
+  const [rate, setRate] = useState(undefined);
+  const [overview, setOverview] = useState("");
+
+  const onChange = (e) => {
+    switch (e.target.name) {
+      case "title":
+        setTitle(e.target.name);
+
+        break;
+      case "artist":
+        setArtist(e.target.name);
+
+        break;
+      case "overview":
+        setOverview(e.target.name);
+
+        break;
+      case "genre":
+        setGenre(e.target.name);
+
+        break;
+      case "rate":
+        var value = e.target.value;
+        if (value > 5) value = 5;
+        else if (value < 0) value = 1;
+        console.log(value);
+        setRate(value);
+
+        break;
+    }
+  };
+
+  const onSubmitPosting = (e) => {
+    e.preventDefault();
+    if (title === "") {
+      return window.alert("노래 제목을 입력해주세요");
+    }
+    if (genre === "") {
+      return window.alert("장르를 선택해주세요");
+    }
+    if (artist === "") {
+      return window.alert("아티스트 정보를 입력해주세요");
+    }
+
+    if (rate === undefined) {
+      return window.alert("평점을 입력해주세요");
+    }
+    if (overview === "") {
+      return window.alert("노래 정보를 입력해주세요");
+    }
+
+    axios
+
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}/post/create`,
+        { title, genre, artist, overview },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        window.alert("글 작성이 정상적으로 처리되었습니다");
+      });
+  };
+
   return (
     <Posting_page>
       <Posting_page_title>New Post</Posting_page_title>
@@ -114,32 +181,64 @@ const Postingpage = () => {
           type="text"
           name="title"
           placeholder="Title is..."
+          onChange={onChange}
         ></Music_title>
         <Genre_field>
           <Genre_title>Genres</Genre_title>
           <Checkbox_container>
-            <Music_genre type="checkbox" id="cb1" value="pop" name="pop" />
+            <Music_genre
+              onChange={onChange}
+              type="radio"
+              id="cb1"
+              value="POP"
+              name="genre"
+            />
             Pop
-            <Music_genre type="checkbox" id="cb2" value="kpop" name="kpop" />
+            <Music_genre
+              onChange={onChange}
+              type="radio"
+              id="cb2"
+              value="KPOP"
+              name="genre"
+            />
             K-Pop
-            <Music_genre type="checkbox" id="cb3" value="rock" name="rock" />
+            <Music_genre
+              onChange={onChange}
+              type="radio"
+              id="cb3"
+              value="ROCK"
+              name="genre"
+            />
             Rock
-            <Music_genre type="checkbox" id="cb4" value="jazz" name="jazz" />
+            <Music_genre
+              onChange={onChange}
+              type="radio"
+              id="cb4"
+              value="JAZZ"
+              name="genre"
+            />
             Jazz
             <Music_genre
-              type="checkbox"
+              onChange={onChange}
+              type="radio"
               id="cb5"
-              value="hiphop"
-              name="hiphop"
+              value="HIPHOP"
+              name="genre"
             />
             Hiphop
-            <Music_genre type="checkbox" id="cb6" value="disco" name="disco" />
+            <Music_genre
+              onChange={onChange}
+              type="radio"
+              id="cb6"
+              value="DISCO"
+              name="genre"
+            />
             Disco
             <Music_genre
-              type="checkbox"
+              type="radio"
               id="cb7"
-              value="electronic"
-              name="electronic"
+              value="ELECTRONIC"
+              name="genre"
             />
             Electronic Music
           </Checkbox_container>
@@ -151,16 +250,20 @@ const Postingpage = () => {
               type="text"
               name="artist"
               placeholder=" Artist is..."
+              onChange={onChange}
             ></Music_artist>
           </Music_artist_area>
 
           <Music_rate_area>
             <Music_rate_title>Rates</Music_rate_title>
             <Music_rate
+              onChange={onChange}
               type="number"
-              min="1"
-              max="5"
-              step="0.5"
+              min={1}
+              max={5}
+              step={0.5}
+              name="rate"
+              value={rate}
               placeholder=" */5.0"
             ></Music_rate>
           </Music_rate_area>
@@ -169,9 +272,10 @@ const Postingpage = () => {
           id="text"
           name="overview"
           placeholder="This music is..."
+          onChange={onChange}
           maxLength="500"
         ></Music_text>
-        <Submit_btn>Submit</Submit_btn>
+        <Submit_btn onClick={onSubmitPosting}>Submit</Submit_btn>
       </Posting_page_form>
     </Posting_page>
   );
