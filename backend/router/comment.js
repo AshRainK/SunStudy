166663fc1ce1932d1283f22128f4e45784f0d0b5
Suneom <dkxl0701@gmail.com>
2022-02-17 21:46:49 +0900

    Updated comment router

--- backend/router/comment.js
+++ backend/router/comment.js
@@ -6,16 +6,14 @@ const db = require("../lib/database");
 //Create
 router.post("/create", (req, res, next) => {
   // DB에 저장하고 쿼리문을 통해 결과물 전송
-  /*
-    if(!util.IsOwner(req,res)){
-        return res.status(400).send({code : 400, payload : '로그인이 필요합니다.'});
-    }
-    */
+  if (!util.IsOwner(req, res)) {
+    return res.status(400).send({ code: 400, payload: "로그인이 필요합니다." });
+  }
   const { post_num, comment } = req.body;
   db.query(
     `INSERT INTO comment(comment,written_date,post_num,id)
     VALUES(?,NOW(),?,?)`,
-    [comment, post_num, 201710939],
+    [comment, post_num, req.user.id],
     (err) => {
       if (err) {
         next(err);
@@ -54,9 +52,7 @@ router.patch("/update", (req, res, next) => {
         next(err);
       }
     });
-    db.query(`SELECT post_num FROM comment WHERE comment_num = ?`,
-    [comment_num],
-    (err,result)=>{
+    db.query(`SELECT post_num FROM comment WHERE comment_num = ?`, [comment_num], (err, result) => {
       db.query(
         `SELECT post_num,comment_num,comment,written_date,updated_date,comment.id as commenter,nickname FROM comment LEFT JOIN user on comment.id = user.id WHERE post_num = ? ORDER BY written_date DESC`,
         [result[0].post_num],
@@ -67,7 +63,7 @@ router.patch("/update", (req, res, next) => {
           res.status(200).send({ code: 200, payload: result });
         }
       );
-    })
+    });
     //}
   });
 });
@@ -86,24 +82,23 @@ router.delete("/:comment_num", (req, res, next) => {
     //if(result[0].commenter !== 201710939){
     //return res.status(400).send({code : 400, payload : "다른 사용자가 작성한 댓글입니다."})
     //} else {
-  db.query(`SELECT post_num FROM comment WHERE comment_num = ?`,
-  [req.params.comment_num],
-  (err,results)=>{
-    db.query(`DELETE FROM comment WHERE comment_num = ?`, [req.params.comment_num], (err) => {
-      if (err) {
-        next(err);
-      }
+    db.query(`SELECT post_num FROM comment WHERE comment_num = ?`, [req.params.comment_num], (err, results) => {
+      db.query(`DELETE FROM comment WHERE comment_num = ?`, [req.params.comment_num], (err) => {
+        if (err) {
+          next(err);
+        }
+      });
+      db.query(
+        `SELECT post_num,comment_num,comment,written_date,updated_date,comment.id as commenter,nickname FROM comment LEFT JOIN user on comment.id = user.id WHERE post_num = ? ORDER BY written_date DESC`,
+        [results[0].post_num],
+        (err, result) => {
+          if (err) {
+            next(err);
+          }
+          res.status(200).send({ code: 200, payload: result });
+        }
+      );
     });
-    db.query(
-    `SELECT post_num,comment_num,comment,written_date,updated_date,comment.id as commenter,nickname FROM comment LEFT JOIN user on comment.id = user.id WHERE post_num = ? ORDER BY written_date DESC`,
-    [results[0].post_num],
-    (err, result) => {
-      if (err) {
-        next(err);
-      }
-      res.status(200).send({ code: 200, payload: result });
-    });
-  });
     //}
   });
 });
