e52f65e8cb5d6e9d42409d55adb303afc75a1fdf
JW <kjo98@naver.com>
2022-03-12 20:33:42 +0900

    add url to post table & add url to post router

--- backend/router/search.js
+++ backend/router/search.js
@@ -8,7 +8,7 @@ router.get('/',(req,res,next)=>{
     let body = req.query.body ? "%"+req.query.body+"%" : "%%";
     let genre = req.query.genre ? req.query.genre : "%%";
     let rating = req.query.rate ? req.query.rate : 0;
-    db.query('SELECT post.post_num,artist,title,rating,post_body,created_date,post.updated_date,post.id as writer_id,post.genre,post.nickname,count(comment.post_num) as comment_count FROM (SELECT post.post_num,artist,title,rating,post_body,created_date,post.updated_date,post.id,post.genre,nickname FROM post LEFT  JOIN user on post.id = user.id) AS post LEFT JOIN comment on post.post_num = comment.post_num WHERE title LIKE ? and artist LIKE ? and post_body LIKE ? and genre LIKE ? and rating >= ? GROUP BY post_num  ORDER BY post.created_date DESC',
+    db.query('SELECT post.post_num,artist,title,rating,url,post_body,created_date,post.updated_date,post.id as writer_id,post.genre,post.nickname,count(comment.post_num) as comment_count FROM (SELECT post.post_num,artist,title,rating,url,post_body,created_date,post.updated_date,post.id,post.genre,nickname FROM post LEFT  JOIN user on post.id = user.id) AS post LEFT JOIN comment on post.post_num = comment.post_num WHERE title LIKE ? and artist LIKE ? and post_body LIKE ? and genre LIKE ? and rating >= ? GROUP BY post_num  ORDER BY post.created_date DESC',
     [title,artist,body,genre,rating],
     (err,result)=>{
         if(err){
