4c5cd6180213cab09dfb06da7650ebde678309f0
Suneom <dkxl0701@gmail.com>
2022-03-25 00:01:22 +0900

    Update post router

--- backend/router/post.js
+++ backend/router/post.js
@@ -93,6 +93,23 @@ router.delete("/:post_num", (req, res, next) => {
   });
 });
 
+//User Post
+router.get("/user", (req, res, next) => {
+  if (!util.IsOwner(req, res)) {
+    return res.status(400).send({ code: 400, payload: "로그인이 필요합니다." });
+  }
+
+  db.query(
+    `SELECT post.post_num,artist,title,rating,url,post_body,created_date,post.updated_date,post.id as writer_id,post.genre,post.nickname,count(comment.post_num) as comment_count FROM (SELECT post.post_num,artist,rating,title,url,post_body,created_date,post.updated_date,post.id,post.genre,nickname FROM post LEFT  JOIN user on post.id = user.id) AS post LEFT JOIN comment on post.post_num = comment.post_num where post.id = ${req.user.id} GROUP BY post_num ORDER BY post_num DESC `,
+    (err, result) => {
+      if (err) {
+        next(err);
+      }
+      res.status(200).send({ code: 200, payload: result });
+    }
+  );
+});
+
 //READ
 router.get("/:post_num", (req, res, next) => {
   db.query(
